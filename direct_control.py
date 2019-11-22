54b5527a018f767386ca82892dddaa8ab74a9166
declanoller <declanoller@gmail.com>
2019-11-21 22:44:32 -0500

    added plot scripts

--- direct_control.py
+++ direct_control.py
@@ -23,31 +23,45 @@ class curses_env:
     def DCloop(self, stdscr):
         #https://docs.python.org/3/howto/curses.html
         #https://docs.python.org/3/library/curses.html#curses.window.clrtobot
-        print('Size of curses window: LINES={}, COLS={}'.format(curses.LINES, curses.COLS), 1)
+
+        curses.halfdelay(1)
+
+        #print('Size of curses window: LINES={}, COLS={}'.format(curses.LINES, curses.COLS), 1)
         delay_time = 0.05
 
         torque = 0.0
         torque_incr = 0.1
+        max_torque = 2.0
 
-        last_action_str_pos = [0, 10]
-        waiting_str_pos = [0, 12]
+        last_action_str_pos = [0, 8]
+        waiting_str_pos = [0, 10]
         torque_str_pos = [0, 6]
 
         x_str_pos = [0, 2]
         y_str_pos = [0, 3]
         thetadot_str_pos = [0, 4]
 
+
+        stay_on = False
+
+
         self.drawStandard(stdscr)
 
         while True:
             c = stdscr.getch()
 
+            #_, _, _ = self.env._get_obs()
+
             if c != curses.ERR:
 
                 if c == curses.KEY_LEFT:
-                    torque += torque_incr
+                    curses.flushinp()
+
+                    torque = min(max_torque, torque + torque_incr)
                     obs, reward, done, info = self.env.step([torque])
                     x, y, thetadot = obs
+
+                    stay_on = True
 
                     time.sleep(delay_time)
                     self.drawStandard(stdscr)
@@ -63,9 +77,13 @@ class curses_env:
 
 
                 if c == curses.KEY_RIGHT:
-                    torque -= torque_incr
+                    curses.flushinp()
+
+                    torque = max(-max_torque, torque - torque_incr)
                     obs, reward, done, info = self.env.step([torque])
                     x, y, thetadot = obs
+
+                    stay_on = True
 
                     time.sleep(delay_time)
                     self.drawStandard(stdscr)
@@ -79,11 +97,63 @@ class curses_env:
                     stdscr.addstr(last_action_str_pos[1], last_action_str_pos[0], 'Pressed Right key, decrementing torque')
                     self.moveCursorRefresh(stdscr)
 
-                if c == ord('r'):
 
+
+
+
+                if c == curses.KEY_UP:
+                    curses.flushinp()
+
+                    torque = 2
+                    obs, reward, done, info = self.env.step([torque])
+                    x, y, thetadot = obs
+
+                    stay_on = False
+
+                    time.sleep(delay_time)
+                    self.drawStandard(stdscr)
+
+                    stdscr.addstr(x_str_pos[1], x_str_pos[0], 'x = {:.2f}'.format(x))
+                    stdscr.addstr(y_str_pos[1], y_str_pos[0], 'y = {:.2f}'.format(y))
+                    stdscr.addstr(thetadot_str_pos[1], thetadot_str_pos[0], 'thetadot = {:.2f}'.format(thetadot))
+
+                    stdscr.addstr(torque_str_pos[1], torque_str_pos[0], 'Current torque = {:.2f}'.format(torque))
+
+                    stdscr.addstr(last_action_str_pos[1], last_action_str_pos[0], 'Pressed Down key, applying momentary max torque')
+                    self.moveCursorRefresh(stdscr)
+
+
+                if c == curses.KEY_DOWN:
+                    curses.flushinp()
+
+                    torque = -2
+                    obs, reward, done, info = self.env.step([-2])
+                    x, y, thetadot = obs
+
+                    stay_on = False
+
+                    time.sleep(delay_time)
+                    self.drawStandard(stdscr)
+
+                    stdscr.addstr(x_str_pos[1], x_str_pos[0], 'x = {:.2f}'.format(x))
+                    stdscr.addstr(y_str_pos[1], y_str_pos[0], 'y = {:.2f}'.format(y))
+                    stdscr.addstr(thetadot_str_pos[1], thetadot_str_pos[0], 'thetadot = {:.2f}'.format(thetadot))
+
+                    stdscr.addstr(torque_str_pos[1], torque_str_pos[0], 'Current torque = {:.2f}'.format(torque))
+
+                    stdscr.addstr(last_action_str_pos[1], last_action_str_pos[0], 'Pressed Up key, applying momentary -max torque')
+                    self.moveCursorRefresh(stdscr)
+
+
+
+                if c == ord('r'):
+                    curses.flushinp()
+                    
                     torque = 0.0
                     obs, reward, done, info = self.env.step([torque])
                     x, y, thetadot = obs
+
+                    stay_on = False
 
                     time.sleep(delay_time)
                     self.drawStandard(stdscr)
@@ -98,24 +168,40 @@ class curses_env:
                     self.moveCursorRefresh(stdscr)
 
 
-                elif c == ord('q'):
+                if c == ord('q') or c == 27:
+                    stdscr.addstr(last_action_str_pos[1], last_action_str_pos[0], 'You pressed q! exiting.')
                     print('you pressed q! exiting', 1)
                     break  # Exit the while loop
 
 
             else:
+
                 x, y, thetadot = self.env._get_obs()
 
                 self.drawStandard(stdscr)
+
+                if not stay_on:
+                    torque = 0
+                    obs, reward, done, info = self.env.step([torque])
+                    stdscr.addstr(torque_str_pos[1], torque_str_pos[0], 'Current torque = {:.2f}'.format(torque))
+                    curses.flushinp()
+
 
                 stdscr.addstr(x_str_pos[1], x_str_pos[0], 'x = {:.2f}'.format(x))
                 stdscr.addstr(y_str_pos[1], y_str_pos[0], 'y = {:.2f}'.format(y))
                 stdscr.addstr(thetadot_str_pos[1], thetadot_str_pos[0], 'thetadot = {:.2f}'.format(thetadot))
 
-                stdscr.addstr(torque_str_pos[1], torque_str_pos[0], 'Current torque = {:.2f}'.format(torque))
-
                 stdscr.addstr(waiting_str_pos[1], waiting_str_pos[0], 'Waiting for next command...')
                 self.moveCursorRefresh(stdscr)
+
+
+
+    def draw_directions(self, stdscr):
+
+        stdscr.addstr(curses.LINES - 4, 0, '--Press Up and Down for momentary max torque')
+        stdscr.addstr(curses.LINES - 3, 0, '--Press Left and Right to adjust permanent applied torque')
+        stdscr.addstr(curses.LINES - 2, 0, '--Press r to refresh')
+        stdscr.addstr(curses.LINES - 1, 0, '--Press q to quit')
 
 
     def moveCursorRefresh(self, stdscr):
@@ -134,7 +220,7 @@ class curses_env:
     def drawStandard(self, stdscr):
         stdscr.erase()
 
-        stdscr.addstr(curses.LINES - 1, 0,  'Press q or Esc to quit')
+        self.draw_directions(stdscr)
 
         stdscr.refresh() #Do this after addstr
 
